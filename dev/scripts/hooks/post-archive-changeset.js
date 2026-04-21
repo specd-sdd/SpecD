e917e80cb1f2abb5cb98a8a60be173919598dd8f
Manuel Lozano García <1839992+lsmonki@users.noreply.github.com>
2026-04-21 07:06:30 +0200

    chore(root): update post-archive hooks script

--- dev/scripts/hooks/post-archive-changeset.js
+++ dev/scripts/hooks/post-archive-changeset.js
@@ -5,7 +5,7 @@ import path from 'node:path'
 import { fileURLToPath } from 'node:url'
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
-const ROOT = path.resolve(__dirname, '..', '..')
+const ROOT = path.resolve(__dirname, '..', '..', '..')
 
 const WORKSPACE_PACKAGE_MAP = {
   'core': '@specd/core',
